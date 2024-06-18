9fb9a1df31e3966fb7851e2fc045a5ca44597be6
Luizrebelatto <lbrebelatto@gmail.com>
2024-06-18 20:09:22 -0300

    Initial Commit

---
+++ App-weather-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App-weather-ios
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 18/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
