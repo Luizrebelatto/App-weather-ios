72aa4ce7743d1a1429ea247cf231e209fc7f2dcd
Luizrebelatto <lbrebelatto@gmail.com>
2024-06-19 14:36:13 -0300

    feat: create header view
    - text city
    - text temperature
    - svg image

--- App-weather-ios/ViewController.swift
+++ App-weather-ios/ViewController.swift
@@ -16,6 +16,43 @@ class ViewController: UIViewController {
         imageBackground.image = UIImage(named: "background")
         return imageBackground
     }()
+    
+    private lazy var headerView: UIView = {
+        let header = UIView(frame: .zero)
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.backgroundColor = .white
+        header.clipsToBounds = true
+        header.layer.cornerRadius = 20
+        return header
+    }()
+    
+    private lazy var cityLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Cachoeirinha"
+        label.textColor = UIColor(named: "primaryText")
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var temperatureLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "25°C"
+        label.textColor = UIColor(named: "primaryText")
+        label.font = UIFont.systemFont(ofSize: 70, weight: .bold)
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var weatherIcon: UIImageView = {
+        let image = UIImageView(frame: .zero)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "sunIcon")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +66,10 @@ class ViewController: UIViewController {
     
     private func setHierarchy(){
         view.addSubview(backgroundView)
+        view.addSubview(headerView)
+        headerView.addSubview(cityLabel)
+        headerView.addSubview(temperatureLabel)
+        headerView.addSubview(weatherIcon)
     }
     
     private func setConstraints(){
@@ -37,6 +78,33 @@ class ViewController: UIViewController {
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            headerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -35),
+            headerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 35),
+            headerView.heightAnchor.constraint(equalToConstant: 169)
+        ])
+        
+        NSLayoutConstraint.activate([
+            cityLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 15),
+            cityLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 15),
+            cityLabel.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -15)
+        ])
+        
+        NSLayoutConstraint.activate([
+            temperatureLabel.topAnchor.constraint(equalTo: cityLabel.topAnchor, constant: 31),
+            temperatureLabel.leadingAnchor.constraint(equalTo: cityLabel.leadingAnchor, constant: 20),
+            temperatureLabel.trailingAnchor.constraint(equalTo: cityLabel.trailingAnchor, constant: -20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            weatherIcon.heightAnchor.constraint(equalToConstant: 86),
+            weatherIcon.widthAnchor.constraint(equalToConstant: 86),
+            weatherIcon.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -26),
+            weatherIcon.centerYAnchor.constraint(equalTo: temperatureLabel.centerYAnchor),
+            weatherIcon.leadingAnchor.constraint(equalTo: temperatureLabel.trailingAnchor, constant: 15)
         ])
     }
 
