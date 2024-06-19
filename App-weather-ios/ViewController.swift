3cc049f082210d48d33d9af77110576fb4e860b4
Luizrebelatto <lbrebelatto@gmail.com>
2024-06-18 23:54:04 -0300

    feat: create and config background

--- App-weather-ios/ViewController.swift
+++ App-weather-ios/ViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var backgroundView: UIImageView = {
+        let imageBackground = UIImageView(frame: .zero)
+        imageBackground.translatesAutoresizingMaskIntoConstraints = false
+        imageBackground.contentMode = .scaleAspectFill
+        imageBackground.image = UIImage(named: "background")
+        return imageBackground
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
-        // Do any additional setup after loading the view.
+        setupView()
+    }
+    
+    private func setupView(){
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        view.addSubview(backgroundView)
+    }
+    
+    private func setConstraints(){
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
 
 }
