a9578b6839617d7877071cb7757b7c546a1a2e6f
Luizrebelatto <lbrebelatto@gmail.com>
2024-06-26 10:16:18 -0300

    feat: create structs app

---
+++ App-weather-ios/Models/DailyForecasts.swift
@@ -0,0 +1,15 @@
+//
+//  DailyForecasts.swift
+//  App-weather-ios
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 26/06/24.
+//
+
+import Foundation
+
+struct DailyForecasts {
+    let day: String
+    let minTemperature: String
+    let maxTemperature: String
+    let iconTemperature: String
+}
---
+++ App-weather-ios/Models/HourlyForecasts.swift
@@ -0,0 +1,14 @@
+//
+//  HourlyForecasts.swift
+//  App-weather-ios
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 26/06/24.
+//
+
+import Foundation
+
+struct HourlyForecasts {
+    let hour: String
+    let temperature: String
+    let iconForecast: String
+}
